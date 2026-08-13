6258e5b5fe7adf9fd3897eacdac34f1ebccf67df
Angelina <angelina18765@gmail.com>
2026-08-13 22:55:51 +1000

    Time: 0 ms (100%), Space: 23.1 MB (31.89%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,17 +1,13 @@
+from collections import Counter
+
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        count = {}      # dict to store number and occurrence
-
-        for num in nums:
-            count[num] = count.get(num, 0) + 1
-
-        # sort the dict's items by count, descending
-        sorted_items = sorted(count.items(), key=lambda x: x[1], reverse=True)
-
-        result = []
-        for i in range(k):
-            result.append(sorted_items[i][0])
-
-        return result
+            count = Counter(nums)
+            top_k = count.most_common(k)   # returns [(num, count), ...] already sorted, top k only!
+            
+            result = []
+            for pair in top_k:
+                result.append(pair[0])
+            return result
 
 
